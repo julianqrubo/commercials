34e610674392a87509be6cc07b561debfdffbddb
shareppy <shareppy@shareppy-HP-14-Notebook-PC>
2015-05-31 21:33:57 -0500

    Se agregaron librerias para el pool de conexiones y se implementó login a la base de datos haciendo un query. Se publica archivo de base de datos y export de la tabla de usuarios.

--- src/java/com/cleardev/servlets/Login.java
+++ src/java/com/cleardev/servlets/Login.java
@@ -6,6 +6,7 @@
 package com.cleardev.servlets;
 
 import com.cleardev.config.Params;
+import com.cleardev.db.RowProcessor;
 import com.jolbox.bonecp.BoneCP;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -18,61 +19,42 @@ import java.util.logging.Logger;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.swing.text.DefaultEditorKit;
 
 /**
  *
  * @author shareppy
  */
 @WebServlet(name = "Login", urlPatterns = {"/adm/login"})
-public class Login extends HttpServlet {
+public class Login extends BaseServlet {
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        RequestDispatcher requestDispatcher = request.getRequestDispatcher("login.jsp");
-        requestDispatcher.forward(request, response);
+    protected String getView() {
+        return "login.jsp";
     }
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    protected void post(HttpServletRequest request, HttpServletResponse response, Connection connection)
             throws ServletException, IOException {
         System.out.println("Llego una petición");
         PrintWriter pw = response.getWriter();
         String username = request.getParameter(Params.USERNAME.toString());
         String password = request.getParameter(Params.PASSWORD.toString());
         response.setContentType("text/plain");
-
-        BoneCP pool = (BoneCP) getServletContext().getAttribute(Params.POOL.toString());
-        Connection connection = null;
-
-           boolean into = false;
-        try {
-            connection = pool.getConnection();
-            PreparedStatement prepareStatement = connection.prepareStatement("SELECT   id, names, lastnames, username, password, info FROM security.user WHERE username = ? AND password = ?");
-            prepareStatement.setString(1, username);
-            prepareStatement.setString(2, password);
-            ResultSet executeQuery = prepareStatement.executeQuery();
-            if( executeQuery.next() ){
-                into = true;
+        StringBuilder resp = new StringBuilder("Error de inicio de sesión");
+        query("SELECT   id, names, lastnames, username, password, info FROM security.user WHERE username = ? AND password = ?", new RowProcessor() {
+            @Override
+            public boolean processRow() {
+                Long id = get("id");
+                if( id != null ){
+                    resp.delete(0,resp.length());
+                    resp.append("Bienvenido...");
+                }
+                return false;
             }
-        } catch (SQLException ex) {
-            Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
-        }finally{
-            try {
-                connection.close();
-            } catch (SQLException ex) {
-                Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
-        if(into){
-            pw.println("Bienvenido...");
-        }else{
-                pw.println("Error de inicio de sesión...");
-        }
+        }, username, password);
+        pw.print(resp.toString());
         pw.close();
     }
 
